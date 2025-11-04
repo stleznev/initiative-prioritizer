8b94ad463e7498384002a622d212610438e6a251
Nikita <stleznev@gmail.com>
2025-11-04 19:39:00 +0300

    Update route.ts

--- app/api/admin/upload/route.ts
+++ app/api/admin/upload/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import prisma from '@/lib/prisma';
+import prisma from '../../../../lib/prisma';
 import * as xlsx from 'xlsx';
 
 // Required columns for each row in the Excel file
