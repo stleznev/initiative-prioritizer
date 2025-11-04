04ea56d6f4513b5ca436d2eeb31957647b276d0b
Nikita <stleznev@gmail.com>
2025-11-04 08:10:16 -0800

    Create prisma.ts

---
+++ lib/prisma.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    // Enable logging if you want to debug SQL queries.
+    // log: ['query'],
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
