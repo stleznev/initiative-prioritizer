a5f82130e5fa478b47ffc965cca8742e1b1eeb58
Nikita <stleznev@gmail.com>
2025-11-05 08:56:50 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,6 @@ import prisma from '../lib/prisma';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
-/**
- * Root page.  Checks whether the visitor has a uid cookie and a corresponding
- * user record.  If no valid user is found, the user is redirected to
- * /onboarding to enter their name.  Otherwise, redirect to /compare to
- * start comparing initiatives.
- */
 export default async function Home() {
   const uid = cookies().get('uid')?.value;
   if (!uid) {
